0201877e32ad2128180a3f1a14873325f76567d9
murakami <rapi.remyli@gmail.com>
2017-04-07 15:04:47 +0900

    Update: FacilityList にリンク追加

--- src/components/FacilityList/FacilityList.js
+++ src/components/FacilityList/FacilityList.js
@@ -6,14 +6,16 @@ export default class FacilityList extends React.Component {
   hotelItem(hotel) {
     return (
       <div key={hotel.id}>
-        <h4>{hotel.Name}</h4>
+        <Link to={`/facility/${hotel.id}`}>
+          <h4>{hotel.Name}</h4>
+        </Link>
         <p>{hotel.Description}</p>
       </div>
     );
   }
 
   render() {
-    const { conditions, hotels } = this.props;
+    const { hotels } = this.props;
 
     let items = [];
     hotels.forEach(hotel => {
